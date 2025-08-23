f0870904c95ddd1b410b5f3837a931aad146e0f9
Jose Zacarias Ponce Garcia <zac.ponce.g@gmail.com>
2025-08-22 20:03:29 -0600

    feat: implementar función agregarAmigo con validación

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Variables
+let amigos = [];
 
 function agregarAmigo() {
-    
+    // 1. Capturar el valor del campo de entrada
+    let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value;
+
+    // 2. Validar la entrada 
+    if (nombre.trim() === '') {
+        alert("Por favor, inserte un nombre."); 
+        return; // Detener ejecución
+    }
+
+    // 3. Actualizar el array de amigos
+    amigos.push(nombre.trim());
+    console.log(amigos);
+
+    // 4. Limpiar el campo de entrada
+    inputAmigo.value = '';
 }
